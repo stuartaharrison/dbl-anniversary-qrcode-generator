aa59df8b0d079467efc7cc1e914621c67aed4fab
Stuart Harrison <email@stuart-harrison.com>
2023-06-03 09:14:16 +0100

    fixed bug with getting new date variable

--- helpers/dates.js
+++ helpers/dates.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 const getExpiryTimestamp = (anniversary = 5) => {
     switch (anniversary) {
         case 5:
-            return Date.now.toString(16).split("");
+            return Date.now().toString(16);
         default:
             return moment().valueOf();
     }
